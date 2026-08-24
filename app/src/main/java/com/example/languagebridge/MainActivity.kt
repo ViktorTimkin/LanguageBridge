e669f06c864565686f9efac033f1c871a06e56da
Sky-Net <Nadkrauler@gmail.com>
2026-08-24 18:20:29 +0500

    Create language selection interface

--- app/src/main/java/com/example/languagebridge/MainActivity.kt
+++ app/src/main/java/com/example/languagebridge/MainActivity.kt
@@ -4,14 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.languagebridge.ui.theme.LanguageBridgeTheme
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,11 +41,59 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Language Bridge",
-        modifier = modifier
-    )
+fun Greeting(
+    name: String,
+    modifier: Modifier = Modifier
+) {
+    var sourceLanguage by remember { mutableStateOf("Русский") }
+
+    val translationLanguage =
+        if (sourceLanguage == "Русский") "Հայերեն" else "Русский"
+
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Language Bridge"
+        )
+
+        Text(
+            text = "Язык оригинала"
+        )
+
+        Button(
+            onClick = {}
+        ) {
+            Text(sourceLanguage)
+        }
+
+        Button(
+            onClick = {
+                sourceLanguage =
+                    if (sourceLanguage == "Русский") "Հայերեն" else "Русский"
+            }
+        ) {
+            Text("⇅")
+        }
+
+        Text(
+            text = "Язык перевода"
+        )
+
+        Button(
+            onClick = {}
+        ) {
+            Text(translationLanguage)
+        }
+
+        Button(
+            onClick = {}
+        ) {
+            Text("Продолжить")
+        }
+    }
 }
 
 @Preview(showBackground = true)
